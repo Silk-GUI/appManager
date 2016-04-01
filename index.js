f1fb55656eb4bc2f21673f78b66499b95be2955e
zodern <zodern@users.noreply.github.com>
2016-04-01 16:22:52 -0500

    Get app list

--- index.js
+++ index.js
@@ -15,25 +15,15 @@ var electronApp = require('app');
 var BrowserWindow = require('browser-window');
 var bowerStatic = require('bower_static');
 var Eureca = require('eureca.io');
+var exports = require('./exports.js');
 
 bowerStatic.changeAppRoot(__dirname);
 app.use('/bc', bowerStatic);
 app.use(express.static(path.join(__dirname, 'public')));
 
-//sock js
-var sockjs_opts = { sockjs_url: "http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js" };
-
-var sockjsServer = sockjs.createServer(sockjs_opts);
-sockjsServer.on('connection', function (conn) {
-  conn.on('data', function (message) {
-    conn.write(message);
-  });
-});
-
-sockjsServer.installHandlers(server, { prefix: '/ws' });
-
 var eurecaServer = new Eureca.Server({ transport: 'sockjs' });
 eurecaServer.attach(server);
+eurecaServer.exports = exports;
 
 server.listen(2001, '0.0.0.0');
 
@@ -41,6 +31,8 @@ electronApp.on('ready', function () {
   var window = new BrowserWindow();
   window.loadUrl('http://bing.com');
 });
+
+
 
 return;
 
@@ -83,125 +75,4 @@ methods.add({
   },
   "apps/install": startInstall
 });
-function startInstall(data, call_obj, send) {
-  var path = data.url;
-  // check if path is local or a github repository
-  var localPath = false;
-  if (path.indexOf('/') === 0) {
-    localPath = true;
-  } else if (path.indexOf('~/') === 0) {
-    localPath = true;
-  }
-  if (localPath === false) {
-    download(data, call_obj, send);
-  } else {
-    console.log('adding external app');
-    Silk.api.call('apps/external/add', data.url, function (err, result) {
-      console.log('added external app', err, result);
-    });
-  }
-}
 
-function download(data, call_ob, send) {
-  fs.exists(appsFolder + path.sep + data.url.replace("/", "-"), function (exists) {
-    if (exists === true) {
-      send(new Error("App already exists"));
-      return;
-    }
-
-    if (data.url === "") {
-      send(new Error("Url can not be empty"));
-    }
-    var repo = data.url;
-
-    var url = "https://api.github.com/repos/" + repo + "/zipball";
-    var options = {
-      url: url,
-      headers: {
-        'User-Agent': 'silk-gui'
-      }
-    };
-
-    send(void(0), "Downloading...");
-    request(options).on('response', function (response) {
-    }).on('error', function (err) {
-      send(err)
-    }).on("end", function () {
-      send(void(0), "pending");
-      install(data, call_ob, send)
-    }).pipe(fs.createWriteStream(__dirname + path.sep + 'test.zip'));
-
-  });
-}
-
-function install(data, call_ob, send) {
-  send(void(0), "Installing ...");
-  var extractTo = appsFolder + path.sep + data.url.replace("/", "-");
-  try {
-    yauzl.open(__dirname + path.sep + 'test.zip', function (err, zipfile) {
-      if (err) {
-        send(err);
-        return;
-      }
-      zipfile.once('end', function () {
-        Silk.api.call('apps/start', data.url.replace('/', '-'), function (err, data) {
-          console.log(error);
-          send(void(0), "Finished installing!")
-
-        });
-        send(void(0), 'Starting App');
-        fs.unlink(__dirname + path.sep + 'test.zip', function (err) {
-          if (err) {
-            send(err);
-
-          }
-          console.log('Installed ' + data.url);
-          send(void(0), " ");
-        })
-      });
-      zipfile.on("entry", function (entry) {
-
-        if (/\/$/.test(entry.fileName)) {
-          // directory file names end with '/'
-          return;
-        }
-
-        var fileName = entry.fileName;
-        fileName = fileName.split(path.sep);
-        fileName = fileName.splice(1, fileName.length)
-        fileName = fileName.join(path.sep);
-
-        entry.fileName = fileName;
-        var dest = path.join(extractTo, entry.fileName)
-        var destDir = path.dirname(dest)
-        // dest = dest.split(path.sep);
-        // dest = dest.slice(0, dest.length - 1);
-
-        zipfile.openReadStream(entry, function (err, readStream) {
-          if (err) {
-            send(err);
-            return;
-          }
-
-          mkdirp(destDir, function (err) {
-            if (err) {
-              send(err);
-            }
-
-            //entry.fileName = data.url.replace("/", "-");
-            // ensure parent directory exists, and then:
-
-            readStream.pipe(fs.createWriteStream(dest).on("error", function (err) {
-              send(err)
-            }))
-          });
-
-        });
-
-      });
-    });
-
-  } catch (e) {
-    send(e);
-  }
-}
